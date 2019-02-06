3bf8d00a3145127923b1f061962b0e76d6932fb2
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2019-02-06 20:01:20 +0100

    initial import

---
+++ file-manager-app/src/main/java/org/example/test/FileManagerAppApplication.java
@@ -0,0 +1,14 @@
+package org.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagerAppApplication.class, args);
+	}
+
+}
+
