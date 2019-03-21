89ac973556e608288375d4f6fb80c38a4622549e
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2019-03-21 19:26:38 +0100

    add pages

--- file-manager-dashboard/src/app/module/index/index-routing.module.ts
+++ file-manager-dashboard/src/app/module/index/index-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, children: [
+    { path: 'reports', pathMatch: 'full', redirectTo: ''},
+    { path: '', component: ListComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- file-manager-dashboard/src/app/module/index/index.module.ts
+++ file-manager-dashboard/src/app/module/index/index.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { IndexRoutingModule } from './index-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, ListComponent],
   imports: [
     CommonModule,
     IndexRoutingModule
