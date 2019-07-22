7857b72d874746a9d845d5d26d8991aec27e00a1
Javier Izquierdo Gómez <jizquierdo@indra.es>
2019-07-22 17:04:57 +0200

    Upload Component

--- file-manager-dashboard/src/app/module/index/index.module.ts
+++ file-manager-dashboard/src/app/module/index/index.module.ts
@@ -5,9 +5,10 @@ import { IndexRoutingModule } from './index-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 import { DataTableComponent } from 'src/app/core/components/data-table/data-table.component';
+import { UploadComponent } from './pages/upload/upload.component';
 
 @NgModule({
-  declarations: [HomeComponent, ListComponent, DataTableComponent],
+  declarations: [HomeComponent, ListComponent, DataTableComponent, UploadComponent],
   imports: [
     CommonModule,
     IndexRoutingModule
