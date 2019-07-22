e1a42f222890acb2fd2288675135b5bf610a2936
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2019-07-22 19:00:42 +0200

    routing upload component

--- file-manager-dashboard/src/app/module/index/index-routing.module.ts
+++ file-manager-dashboard/src/app/module/index/index-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
 import { CoreModule } from 'src/app/core/core.module';
+import { UploadComponent } from './pages/upload/upload.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, children: [
-    { path: 'reports', pathMatch: 'full', redirectTo: ''},
-    { path: '', component: ListComponent}
+    { path: '', pathMatch: 'full', redirectTo: 'list'},
+    { path: 'list', component: ListComponent},
+    { path: 'upload', component: UploadComponent}
   ]}
 ];
 
