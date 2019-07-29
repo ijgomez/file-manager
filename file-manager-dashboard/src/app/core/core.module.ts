d6ca5758513f09040d0206795cbc65633d51b6ef
Javier Izquierdo <javier.izquierdo.gomez@gmail.com>
2019-07-29 18:41:54 +0200

    toolbar component

--- file-manager-dashboard/src/app/core/core.module.ts
+++ file-manager-dashboard/src/app/core/core.module.ts
@@ -6,14 +6,15 @@ import { DataTableComponent } from './components/data-table/data-table.component
 import { HeaderPageComponent } from './layout/header-page/header-page.component';
 import { DataTableFilterComponent } from './components/data-table-filter/data-table-filter.component';
 import { DataTablesModule } from 'angular-datatables';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
-  declarations: [DataTableComponent, HeaderPageComponent, DataTableFilterComponent],
+  declarations: [DataTableComponent, HeaderPageComponent, DataTableFilterComponent, ToolbarComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
     DataTablesModule
   ],
-  exports: [HeaderPageComponent, DataTableComponent, DataTableFilterComponent]
+  exports: [HeaderPageComponent, DataTableComponent, DataTableFilterComponent, ToolbarComponent]
 })
 export class CoreModule { }
