6b499f40d1f6aa1e4c48558aabee6e477e69e48b
Javier Izquierdo Gómez <jizquierdo@indra.es>
2019-07-29 13:31:09 +0200

    Filter Component

---
+++ file-manager-dashboard/src/app/core/components/data-table-filter/data-table-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-table-filter',
+  templateUrl: './data-table-filter.component.html',
+  styleUrls: ['./data-table-filter.component.scss']
+})
+export class DataTableFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- file-manager-dashboard/src/app/core/core.module.ts
+++ file-manager-dashboard/src/app/core/core.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import { DataTableComponent } from './components/data-table/data-table.component';
 import { HeaderPageComponent } from './layout/header-page/header-page.component';
+import { DataTableFilterComponent } from './components/data-table-filter/data-table-filter.component';
 
 @NgModule({
-  declarations: [DataTableComponent, HeaderPageComponent],
+  declarations: [DataTableComponent, HeaderPageComponent, DataTableFilterComponent],
   imports: [
     CommonModule,
     CoreRoutingModule
   ],
-  exports: [HeaderPageComponent, DataTableComponent]
+  exports: [HeaderPageComponent, DataTableComponent, DataTableFilterComponent]
 })
 export class CoreModule { }
